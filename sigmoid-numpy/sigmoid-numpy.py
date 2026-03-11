786102eba2bc813ec627bb71e544ce3c3522fb78
Swapnil Gautam <51311257+Swapnil-gautam@users.noreply.github.com>
2026-03-11 17:23:05 -0400

    Update solution for sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -4,7 +4,10 @@ def sigmoid(x):
     """
     Vectorized sigmoid function.
     """
-    # print(x)
+    # the key part was to conver the x to np array once its that then 
+    # we dont need to convert or traverse on each element of the list one by one 
+    # print(x) # [[-1, 0], [1, 2]]
     x = np.asarray(x, dtype=float)
-    # print(x)
+    # print(x) # [[-1.  0.]
+             # [ 1.  2.]]
     return 1/(1+np.exp(-x))
